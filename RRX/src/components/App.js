4c3e4ba00d483979d75014089530149c3e4f3796
weixongxu.xu <weixongxu.xu@qunar.com>
2017-10-17 11:39:35 +0800

    fix: da

--- RRX/src/components/App.js
+++ RRX/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
-//ddddddddd
+
 const App = () => (
   <div>
     <AddTodo />
